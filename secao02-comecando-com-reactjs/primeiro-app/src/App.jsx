2026b0cfb5ec747f6452e0473b08bc5f71eda03f
narakushi <taina.santana1305@gmail.com>
2025-04-14 10:32:32 -0300

    :sparkles: feat: aula 06 - propriedades e components

---
+++ secao02-comecando-com-reactjs/primeiro-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Nome from "./components/Nome";
+
+function App() {
+  return (
+    <>
+      <h1>Componente App</h1>
+      <Nome aluno="Lucas" idade={30}/>
+    </>
+  );
+}
+
+
+export default App;
---
+++ secao02-comecando-com-reactjs/primeiro-app/src/components/Nome.jsx
@@ -0,0 +1,5 @@
+function Nome({aluno, idade}) {
+  return <span>Bem vinda: {aluno} - Idade: {idade} anos</span>;
+}
+
+export default Nome;
