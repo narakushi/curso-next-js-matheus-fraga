449264c0b619c4ba1196ce1127e1d65b66944aa4
narakushi <taina.santana1305@gmail.com>
2025-04-14 10:56:13 -0300

    :feat: aula 07 - trabalhando com state

--- secao02-comecando-com-reactjs/primeiro-app/src/App.jsx
+++ secao02-comecando-com-reactjs/primeiro-app/src/App.jsx
@@ -1,11 +1,23 @@
 import Nome from "./components/Nome";
+import { use, useState } from "react";
 
 function App() {
+  const [aluno, setAluno] = useState('Sujeito Programador');
+
+  function handleChangeName(nome){
+    setAluno(nome);
+  }
+
   return (
-    <>
+    <div>
       <h1>Componente App</h1>
-      <Nome aluno="Lucas" idade={30}/>
-    </>
+      <h2>Olá: {aluno}</h2>
+      <button onClick={() => handleChangeName('Lucas Oliveira')}>
+        Mudar nome
+      </button>
+
+
+    </div>
   );
 }
 
--- secao02-comecando-com-reactjs/primeiro-app/src/components/Nome.jsx
+++ secao02-comecando-com-reactjs/primeiro-app/src/components/Nome.jsx
@@ -1,5 +1,9 @@
+
+
 function Nome({aluno, idade}) {
-  return <span>Bem vinda: {aluno} - Idade: {idade} anos</span>;
+
+
+  return (<span>Bem vinda: {aluno} - Idade: {idade} anos</span>);
 }
 
 export default Nome;
