964893dcbf2b3c2faf30ddef5b41a0206b355c63
narakushi <taina.santana1305@gmail.com>
2025-04-22 14:41:12 -0300

    :sparkles: feat: aula 08 - trabalhando com formulario

--- secao02-comecando-com-reactjs/primeiro-app/src/App.jsx
+++ secao02-comecando-com-reactjs/primeiro-app/src/App.jsx
@@ -1,25 +1,70 @@
-import Nome from "./components/Nome";
 import { use, useState } from "react";
 
 function App() {
-  const [aluno, setAluno] = useState('Sujeito Programador');
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [idade, setIdade] = useState(0);
 
-  function handleChangeName(nome){
-    setAluno(nome);
+  const [user, setUser] = useState({});
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    alert('Usuario registrado com sucesso!');
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email
+    })
   }
 
   return (
     <div>
-      <h1>Componente App</h1>
-      <h2>Olá: {aluno}</h2>
-      <button onClick={() => handleChangeName('Lucas Oliveira')}>
-        Mudar nome
-      </button>
+      <h1>Cadastrando usuário</h1>
+      <form onSubmit={handleRegister}>
+        <label htmlFor="">Nome:</label>
+        <br />
+        <input
+          placeholder="Digite seu nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <br />
 
+        <label htmlFor="">Email:</label>
+        <br />
+        <input
+          placeholder="Digite seu email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
 
+        <label htmlFor="">Idade:</label>
+        <br />
+        <input
+          placeholder="Digite sua idade"
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+        />
+        <br />
+
+        <button type="submit">Registrar</button>
+        
+        <br />
+        <br />
+
+        <div>
+          <span>Bem vindo: {user.nome}</span>
+          <br />
+          <span>Idade: {user.idade}</span>
+          <br />
+          <span>Email: {user.email}</span>
+          <br />
+        </div>
+      </form>
     </div>
   );
 }
-
 
 export default App;
