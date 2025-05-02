c50e83b343a47b76527fe827ccc8f5b731d8ecba
narakushi <taina.santana1305@gmail.com>
2025-05-02 18:32:43 -0300

    :sparkles: feat: aula 27 exibindo issues

--- secao03-primeiro-projeto/repos/src/pages/Repositorio/index.jsx
+++ secao03-primeiro-projeto/repos/src/pages/Repositorio/index.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { Container, Owner, Loading, BackButton } from "./styles";
+import { Container, Owner, Loading, BackButton, IssuesList } from "./styles";
 import { useState, useEffect } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import api from "../../services/api";
@@ -33,7 +33,7 @@ export default function Repositorio() {
       console.log(repo);
 
       setIssues(issuesData.data);
-      console.log(issuesData);
+      console.log(issues);
       setLoading(false);
     };
 
@@ -58,6 +58,26 @@ export default function Repositorio() {
         <h1>{repo.name}</h1>
         <p>{repo.description}</p>
       </Owner>
+
+      <IssuesList>
+        {issues.map((issue) => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map((label) => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
     </Container>
   );
 }
