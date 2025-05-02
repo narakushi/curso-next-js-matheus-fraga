25d9b5c4b72b6d06d22b876cac636b970f4328d9
narakushi <taina.santana1305@gmail.com>
2025-05-02 15:48:51 -0300

    :sparkles: feat: aula25 - carregando detalhes

--- secao03-primeiro-projeto/repos/src/pages/Repositorio/index.jsx
+++ secao03-primeiro-projeto/repos/src/pages/Repositorio/index.jsx
@@ -1,12 +1,39 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { Container } from "./styles";
+import { useState, useEffect } from "react";
+import api from "../../services/api";
 
 export default function Repositorio() {
+  const { repositorio } = useParams();
 
-  const {repositorio} = useParams();
+  const [repo, setRepo] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  return(
-    <h1 style={{color: '#fff'}}>
-      {repositorio}
-    </h1>
-  )
+  useEffect(() => {
+    const load = async () => {
+      const nomeRepo = repositorio;
+
+      // para não fazer duas chamadas de api com const nome = awat...
+      // podemos usar um Promise.all
+
+      const [respositoriosData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: "open",
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepo(respositoriosData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    };
+
+    load();
+  }, [repositorio]);
+
+  return <Container>{repositorio}</Container>;
 }
