e2e0423d86417814d389a4b6697b32d1e26dd8b1
narakushi <taina.santana1305@gmail.com>
2025-05-03 20:09:01 -0300

    :tada: init: commit do primeiro projeto

---
+++ secao04-entendendo-nextjs/estudo-aula02/src/pages/contato.tsx
@@ -0,0 +1,7 @@
+export default function Contato() {
+  return (
+    <div>
+      <h1>Pagina de contatos</h1>
+    </div>
+  );
+}
---
+++ secao04-entendendo-nextjs/estudo-aula02/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Aprendendo NextJS</h1>
+    </div>
+  );
+}
