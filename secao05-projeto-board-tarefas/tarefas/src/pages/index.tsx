4ca9b75dd75ef94ad109b70c77be0d64480861c5
narakushi <taina.santana1305@gmail.com>
2025-05-03 21:12:08 -0300

    :tada: init: organizando projeto e criando home

---
+++ secao05-projeto-board-tarefas/tarefas/src/pages/index.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import styles from "../styles/home.module.css";
+import Image from "next/image";
+import heroImg from "../../public/assets/hero.png";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="Logo Tarefas+"
+            src={heroImg}
+            priority
+          />
+        </div>
+        <h1 className={styles.title}>
+          Sistemas feito para você organizar <br />
+          seus estudos e tarefas
+        </h1>
+      </main>
+    </div>
+  );
+}
