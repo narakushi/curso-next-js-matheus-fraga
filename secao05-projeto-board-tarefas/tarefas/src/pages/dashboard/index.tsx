4f8af13931042b2ec8847b35718d6e4915cb19db
narakushi <taina.santana1305@gmail.com>
2025-05-05 16:07:38 -0300

    :sparkles: feat: aula 38 - criando rota de admin

--- secao05-projeto-board-tarefas/tarefas/src/pages/dashboard/index.tsx
+++ secao05-projeto-board-tarefas/tarefas/src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from "next";
 import styles from "./styles.module.css";
 import Head from "next/head";
+import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
   return (
@@ -12,3 +14,22 @@ export default function Dashboard() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  // console.log(session);
+
+  if (!session?.user) {
+    // Se não tem usuario vamos redirecionar para "/" (home)
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
